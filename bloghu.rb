b50bb56694885fe6e2d2bcf6af4e9aa0d913b0b4
Marton Tasnadi <marton.tasnadi@gmail.com>
2019-01-13 21:02:08 +0100

    Splitting code into functions lol

--- bloghu.rb
+++ bloghu.rb
@@ -1,13 +1,41 @@
 require "nokogiri"
 require "httparty"
 
-(0...1187).each do | page |
-	Nokogiri::HTML(HTTParty.get("https://hogyvolt.blog.hu/page/" + page.to_s)).css("h1.title > a").each do |link|
-		article = Nokogiri::HTML(HTTParty.get(link["href"]))
-		puts "Title: " + article.css("h1.title > a").text
-		puts "Date: " + article.css("article.post span.date").text
-		puts "Author: " + article.css("article.post span.author > a").text
-		#puts "Content: " + article.css("article.post div.entry").text
-		puts "--------------------"
+def main
+	pager
+end
+
+def get_url
+	"https://hogyvolt.blog.hu"
+end
+
+def build_url page
+	get_url + "/page/" + page.to_s
+end
+
+def pager
+	(0...1187).each do | page |
+		parse_listing_page page
 	end
 end
+
+def parse_listing_page url
+	Nokogiri::HTML(HTTParty.get(build_url(url))).css("h1.title > a").each do |link|
+		parse_article_page link["href"]
+	end
+end
+
+def parse_article_page url
+	article = Nokogiri::HTML(HTTParty.get(url))
+	title = article.css("article.post h1.title > a").text
+	date = article.css("article.post span.date").text
+	author = article.css("article.post span.author > a").text
+	content = article.css("article.post div.entry").text
+
+	puts title
+	puts date
+	puts author
+	#puts content
+end
+
+main
